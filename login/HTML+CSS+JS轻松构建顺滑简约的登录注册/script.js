ecdbe763109e2c7f73a03be0c3ec94cc7f5693e8
sunyctf <changtengfei888@163.com>
2022-04-22 23:45:27 +0800

    新增实例

---
+++ login/HTML+CSS+JS轻松构建顺滑简约的登录注册/script.js
@@ -0,0 +1,53 @@
+/*
+    Logic：
+        主要采用原生 JavaScript，
+        只有在发送 Ajax 请求是才使用 JQuery
+    
+    ===
+    1、登录注册页面的切换逻辑
+
+    2、Ajax发送及接受响应逻辑
+    ===
+*/
+
+
+// 封装选择器, 采用ES6箭头函数写法
+const getSelector = ele => {
+    return typeof ele === "string" ? document.querySelector(ele) : "";
+}
+
+
+// 登录注册载入
+
+const containerShow = () => {
+    var show = getSelector(".container")
+    show.className += " container-show"
+}
+
+
+window.onload = containerShow;
+
+
+// 登录注册页切换
+((window, document) => {
+
+    // 登录 -> 注册
+    let toSignBtn = getSelector(".toSign"),
+        toLoginBtn = getSelector(".toLogin")
+        loginBox = getSelector(".login-box"),
+        signBox = getSelector(".sign-box");
+    
+    toSignBtn.onclick = () => {
+        loginBox.className += ' animate_login';
+        signBox.className += ' animate_sign';
+    }
+
+    toLoginBtn.onclick = () => {
+        loginBox.classList.remove("animate_login");
+        signBox.classList.remove("animate_sign");
+    }
+
+
+})(window, document);
+
+// Ajax 请求发送
