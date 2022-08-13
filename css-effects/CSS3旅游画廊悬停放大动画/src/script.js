6c15d5378afa9ad5e1b8c23f1e1ff522b7248842
sunyctf <changtengfei888@163.com>
2022-08-13 10:01:47 +0800

    新增CSS3旅游画廊悬停放大动画

---
+++ css-effects/CSS3旅游画廊悬停放大动画/src/script.js
@@ -0,0 +1,24 @@
+(function(window, document, $, undefined) {
+	var $slides, $btnArr;
+
+	function onClick(e) {
+		var $target = $(e.target);
+		if ($target.hasClass('slide') && !$target.hasClass('active') && !$target.siblings().hasClass('active')) {
+			$target.removeClass('anim-in last-viewed').addClass('active')
+			$target.siblings().removeClass('anim-in last-viewed').addClass('anim-out');
+		}
+	}
+
+	function closeSlide(e) {
+		var $slide = $(e.target).parent();
+		$slide.removeClass('active anim-in').addClass('last-viewed');
+		$slide.siblings().removeClass('anim-out').addClass('anim-in');
+	}
+
+	$(function() {
+		$slides = $('.slide');
+		$btnArr = $slides.find('.btn-close');
+		$slides.on('click', onClick);
+		$btnArr.on('click', closeSlide);
+	});
+})(this, document, jQuery);
